7da98213e69b48a5cd5e610141fa8b055f944654
Iryna Shashkevych <lanabanana2801@gmail.com>
2026-02-24 12:47:56 +0100

    push

---
+++ src/js/render-functions.js
@@ -0,0 +1,54 @@
+import SipmpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+const galleryEl = document.querySelector('.gallery');
+const loaderEL = document.querySelector('.loader');
+
+const lightbox = new SipmpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
+
+export function createGallery(images) {
+  const markup = images
+    .map(
+      ({
+        webformatURL,
+        largeImageURL,
+        tags,
+        likes,
+        views,
+        comments,
+        downloads,
+      }) => `
+    <li class="gallery-item">
+        <a class="gallery-link" href="${largeImageURL}">
+          <img
+            class="gallery-image"
+            src="${webformatURL}"
+            alt="${tags}"
+            loading="lazy"
+          />
+        </a>
+        <div class="info">
+          <p class="info-item"><b>Likes</b> ${likes}</p>
+          <p class="info-item"><b>Views</b> ${views}</p>
+          <p class="info-item"><b>Comments</b> ${comments}</p>
+          <p class="info-item"><b>Downloads</b> ${downloads}</p>
+        </div>
+      </li>
+`
+    )
+    .join('');
+  galleryEl.insertAdjacentHTML('beforeend', markup);
+  lightbox.refresh();
+}
+
+export function clearGallery() {
+  galleryEl.innerHTML = '';
+}
+
+export function hideLoader() {
+  if (!loaderEL) return;
+  loaderEL.clearList.add('is-hidden');
+}
